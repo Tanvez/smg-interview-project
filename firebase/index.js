634f4a6be3efbdf36b263b3dd70e11d3e6a34ca0
Vesna Tan <tanvezi@gmail.com>
2020-10-17 23:42:21 -0400

    clean up firebase index file

--- firebase/index.js
+++ firebase/index.js
@@ -1,31 +1,5 @@
-// import admin from 'firebase-admin';
-
-// try {
-//   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       project_id: process.env.FIREBASE_PROJECT_ID,
-//       private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-//       client_email: process.env.FIREBASE_CLIENT_EMAIL,
-//     }),
-//     databaseURL: 'https://smg-mobile-test-d177e.firebaseio.com',
-//   });
-// } catch (error) {
-//   /*
-//    * We skip the "already exists" message which is
-//    * not an actual error when we're hot-reloading.
-//    */
-//   if (!/already exists/u.test(error.message)) {
-//     // eslint-disable-next-line no-console
-//     console.error('Firebase admin initialization error', error.stack);
-//   }
-// }
-
-// export default admin.firestore();
 import firebase from 'firebase/app';
-// import 'firebase/auth' // If you need it
-// import 'firebase/firestore' // If you need it
-// import 'firebase/storage' // If you need it
-// import 'firebase/analytics' // If you need it
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
